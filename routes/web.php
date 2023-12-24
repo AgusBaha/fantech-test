e1434b14e985add3dd90377bfcc2d9c1eb58bf9a
AgusBaha <id.stackdevelopers@gmail.com>
2023-12-24 12:12:25 +0700

    feat(role login and user seeder)

--- routes/web.php
+++ routes/web.php
@@ -14,5 +14,35 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('auth.login');
 });
+
+Auth::routes(
+    ['register' => false]
+);
+
+Route::middleware(['auth', 'role:superuser'])->group(function () {
+    Route::get('superadmin', function () {
+        return 'superadmin dashboard';
+    });
+});
+
+Route::middleware(['auth', 'role:sales'])->group(function () {
+    Route::get('sales', function () {
+        return 'sales pages';
+    });
+});
+
+Route::middleware(['auth', 'role:purchase'])->group(function () {
+    Route::get('purchase', function () {
+        return 'purchase pages';
+    });
+});
+
+Route::middleware(['auth', 'role:manager'])->group(function () {
+    Route::get('manager', function () {
+        return 'manager pages';
+    });
+});
+
+Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
