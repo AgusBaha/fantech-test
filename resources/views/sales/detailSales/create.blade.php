e967b63e614173c418ad9e6423626eded77db389
AgusBaha <id.stackdevelopers@gmail.com>
2023-12-25 13:14:58 +0700

    feat(interfaces): select2

--- resources/views/sales/detailSales/create.blade.php
+++ resources/views/sales/detailSales/create.blade.php
@@ -1,5 +1,6 @@
 <x-app-layout title="Sales Details">
     @push('style')
+    <link href="https://cdn.jsdelivr.net/npm/select2@4.1.0-rc.0/dist/css/select2.min.css" rel="stylesheet" />
     @endpush
 
     <!-- Form controls -->
@@ -45,5 +46,14 @@
     </div>
 
     @push('JavaScript')
+    <script src="https://code.jquery.com/jquery-3.6.4.min.js"></script>
+    <script src="https://cdn.jsdelivr.net/npm/select2@4.1.0-rc.0/dist/js/select2.min.js"></script>
+    <script>
+        $(document).ready(function() {
+                $('#inventories_id').select2({
+                    theme: 'bootstrap5',
+                });
+            });
+    </script>
     @endpush
 </x-app-layout>
